23ec849f6421cbfd0814ac8a418a78adb0e31e23
Moussa Sarr <ms2techllc@gmail.com>
2024-12-21 13:02:32 -0500

    added cancel url to  as an api url

---
+++ pages/api/payment/cancel.js
@@ -0,0 +1,9 @@
+// pages/api/payment.js
+
+export default function handler(req, res) {
+    // Set status code and response headers (if needed)
+    res.status(422).json({
+      message: "Something went wrong",
+    });
+  }
+  
