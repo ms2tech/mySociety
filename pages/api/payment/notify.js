28c691c5498d4bc97148fc31bab69764392a917a
Moussa Sarr <ms2techllc@gmail.com>
2024-12-26 17:13:01 -0500

    Need to work on the api endpoints

---
+++ pages/api/payment/notify.js
@@ -0,0 +1,9 @@
+// pages/api/payment.js
+
+export default function handler(req, res) {
+    // Set status code and response headers (if needed)
+    res.status(200).json({
+      message: "Notify",
+    });
+  }
+  
