ee34c7c2e6d669286635da327531c2d059f961aa
Moussa Sarr <ms2techllc@gmail.com>
2024-12-29 19:08:49 -0500

    still testing if notifyUrl works or not

--- pages/api/payment/redirect.js
+++ pages/api/payment/redirect.js
@@ -4,7 +4,11 @@ export default function handler(req, res) {
   // Extract query parameters from the request
   const queryParams = req.query;
 
+   console.log("WE ARE IN SUCCESS URL")
+   console.log("PARFAMS IN SUCCESS URL", queryParams)
+
   // Return query parameters along with a message
+
   res.status(200).json({
     message: "Payment Successful",
     queryParams, // Include the query parameters in the response
