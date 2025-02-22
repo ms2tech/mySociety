0939deb4078228d96b69823162fb97b542a36c43
Moussa Sarr <ms2techllc@gmail.com>
2025-02-22 16:07:02 -0500

    testing more

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -22,7 +22,7 @@ export default function ResetPassword() {
 
     try {
       await account.updateRecovery(userId, secret, password);
-      setSuccess("Password successfully reset! You can now login...");
+      setSuccess("Password successfully reset! You can now go back to the app and login...");
       setTimeout(() => {
         const isMobile = /android|iphone|ipad/i.test(navigator.userAgent);
       
